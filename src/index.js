fc7b9b3004d9884baa2f150c189ad609adb9db9b
deebify <yassine.deeb@outlook.com>
2018-09-22 14:44:43 +0000

    weather with axios -- error with Allow Cross Orgin Access

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import reducers from './reducers'
+import ReduxPromise from 'redux-promise'
+// apply middleware 
+const storeWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={storeWithMiddleware(reducers)}> 
+        <App />
+    </Provider>, document.getElementById('root'));
+
 registerServiceWorker();
